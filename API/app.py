35c96663263461dce4fdbd98835e05f0124140ab
Kanzul Fiqri <62295245+KanzulF@users.noreply.github.com>
2022-06-11 20:14:50 +0700

    upload API plus model
    mengubah code API, menambahkan data dan persiapan untuk menghubungkan model dan API

--- API/app.py
+++ API/app.py
@@ -1,8 +1,13 @@
 # import library
-from urllib import response
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
+import json
+
+from keras_preprocessing import image
+import numpy
+from tensorflow import keras
+
 
 # inisiasi object flask
 app = Flask(__name__)
@@ -14,30 +19,46 @@ api = Api(app)
 CORS(app)
 
 # database
-data = {}
+with open('data.json') as f:
+  data = json.load(f)
 
-# membuat class Resource
+labels = []
+for i in data:
+    labels.append(i)
+
+
 class MyResource(Resource):
     # metode get dan post
     def get(self):
-        # response = {"msg" : "hallo"}
         response = data
         return response
 
     def post(self):
-        nama = request.form["nama"]
-        umur = request.form["umur"]
+        file = request.files['image'].read()
+        # load model
+        model = keras.models.load_model('Modelnew.h5')
+        
+        for ft in file.keys():
+            pathtest = ft
+            img = image.load_img(pathtest, target_size= (224, 224))
+            xtest = image.img_to_array(img)
+            xtest = numpy.expand_dims(xtest, axis=0)
 
-        # memasukkan data
-        data["nama"] = nama
-        data["umur"] = umur
+            file_image = numpy.vstack([xtest])
+            prediction_class = model.predict(file_image, batch_size=10)
 
-        response = {"msg" : "data berhasil dimasukkan"}
-        return response
+            output = prediction_class[0].argmax()
+
+            for i in data:
+                if i == output:
+                    response = data[i]
+            break
+
+        return response,
 
 
 # setup resource
 api.add_resource(MyResource, "/api", methods=["GET", "POST"])
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5005)
+    app.run(debug=True, port=8080)
