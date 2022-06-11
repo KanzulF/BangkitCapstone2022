1c1e79130c998f176037a2760bbc0987f7fd4dd7
Kanzul <kanzulf.16@gmail.com>
2022-06-11 15:33:11 +0000

    change some stuffs

--- API/app.py
+++ API/app.py
@@ -3,10 +3,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import json
-
+from PIL import Image
 from keras_preprocessing import image
 import numpy
 from tensorflow import keras
+from io import BytesIO
 
 
 # inisiasi object flask
@@ -35,24 +36,32 @@ class MyResource(Resource):
 
     def post(self):
         file = request.files['image'].read()
+        # print(file)
         # load model
         model = keras.models.load_model('Modelnew.h5')
+
+        img = Image.open(BytesIO(file))
+        img = img.convert('RGB')
+        img = img.resize((224, 224), Image.NEAREST)
         
-        for ft in file.keys():
-            pathtest = ft
-            img = image.load_img(pathtest, target_size= (224, 224))
-            xtest = image.img_to_array(img)
-            xtest = numpy.expand_dims(xtest, axis=0)
+        # for ft in file.keys():
+        #    pathtest = ft
+        #    img = image.load_img(pathtest, target_size= (224, 224))
+        xtest = image.img_to_array(img)
+        xtest = numpy.expand_dims(xtest, axis=0)
 
-            file_image = numpy.vstack([xtest])
-            prediction_class = model.predict(file_image, batch_size=10)
+        file_image = numpy.vstack([xtest])
+        prediction_class = model.predict(file_image, batch_size=10)
 
-            output = prediction_class[0].argmax()
+        output = prediction_class[0].argmax()
 
-            for i in data:
-                if i == output:
-                    response = data[i]
-            break
+        response = {}
+
+        response = data[list(data.keys())[output]]
+
+        # for i in range(len(data) - 1):
+            # if i == output:
+                # response = data[output]
 
         return response,
 
@@ -61,4 +70,4 @@ class MyResource(Resource):
 api.add_resource(MyResource, "/api", methods=["GET", "POST"])
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, host="0.0.0.0")
